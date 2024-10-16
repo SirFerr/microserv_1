58fe1b4229d5eb4042b0c880ae18eee258c22ad1
sirferr <ursusmartimus007@gmail.com>
2024-10-16 11:27:25 +0300

    added second queue+

--- fanout_consumer.py
+++ fanout_consumer.py
@@ -1,5 +1,6 @@
 import pika
 import time
+import threading
 
 # Параметры подключения к RabbitMQ
 host = '51.250.26.59'
@@ -9,19 +10,24 @@ password = 'guest123'
 
 # Настройка соединения
 credentials = pika.PlainCredentials(login, password)
-connection = pika.BlockingConnection(pika.ConnectionParameters(host, port, '/', credentials))
-channel = connection.channel()
 
-# Объявление обменника типа fanout
-exchange_name = 'logs_fanout'
-channel.exchange_declare(exchange=exchange_name, exchange_type='fanout', durable=True)
+# Объявление обменника типа fanout и очередей
+def setup_channel(queue_name):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host, port, '/', credentials))
+    channel = connection.channel()
+    exchange_name = 'logs_fanout'
 
-# Создание очереди с названием KBO-25-21_Shillo_fanout
-queue_name = 'IKBO-25-21_Shillo_fanout'
-channel.queue_declare(queue=queue_name, durable=True)
+    # Объявление обменника
+    channel.exchange_declare(exchange=exchange_name, exchange_type='fanout', durable=True)
 
-# Связывание очереди с обменником
-channel.queue_bind(exchange=exchange_name, queue=queue_name)
+    # Создание очереди
+    channel.queue_declare(queue=queue_name, durable=True)
+
+    # Связывание очереди с обменником
+    channel.queue_bind(exchange=exchange_name, queue=queue_name)
+
+    return connection, channel
+
 
 # Обработка сообщений с задержкой, зависящей от количества символов #
 def process_message(ch, method, properties, body):
@@ -32,9 +38,28 @@ def process_message(ch, method, properties, body):
     print("Задача выполнена.")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-# Потребление сообщений
-channel.basic_qos(prefetch_count=1)  # Гарантирует, что только одно сообщение будет отправлено потребителю за раз
-channel.basic_consume(queue=queue_name, on_message_callback=process_message)
 
-print(f"Ожидание сообщений из очереди: {queue_name}")
-channel.start_consuming()
+# Настройка обработки сообщений в отдельной очереди
+def start_consuming(queue_name):
+    connection, channel = setup_channel(queue_name)
+
+    # Потребление сообщений
+    channel.basic_qos(prefetch_count=1)  # Гарантирует, что только одно сообщение будет отправлено потребителю за раз
+    channel.basic_consume(queue=queue_name, on_message_callback=process_message)
+
+    print(f"Ожидание сообщений из очереди: {queue_name}")
+    channel.start_consuming()
+
+# Запуск двух потоков для каждой очереди
+if __name__ == '__main__':
+    queue1 = 'IKBO-25-21_Shillo_fanout_1'
+    queue2 = 'IKBO-25-21_Shillo_fanout_2'
+
+    thread1 = threading.Thread(target=start_consuming, args=(queue1,))
+    thread2 = threading.Thread(target=start_consuming, args=(queue2,))
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
